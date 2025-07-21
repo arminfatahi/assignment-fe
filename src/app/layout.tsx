26ad93e807de449eafb2eb34a7c8061827d2e128
arminfatahi <arminfatahi2002@gmail.com>
2025-07-21 17:48:08 +0330

    remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeProvider theme={theme} defaultMode="system">
+        <ThemeProvider theme={theme}>
           <CssBaseline />
           {children}
         </ThemeProvider>
