3ed86b0a224956e4f73c78cc14af439a2e5c2ebb
arminfatahi <arminfatahi2002@gmail.com>
2025-07-24 11:01:08 +0330

    fix: projects is object in json

--- src/lib/HandleProjects.ts
+++ src/lib/HandleProjects.ts
@@ -1,11 +1,15 @@
 import { Project } from "./Types";
 
+type ProjectsData = {
+  projects: Project[];
+};
+
 export async function getProjects(): Promise<Project[] | null> {
   try {
     const res = await fetch("/api/projects");
     if (res.ok) {
-      const projects: Project[] = await res.json();
-      return projects;
+      const projects: ProjectsData = await res.json();
+      return projects.projects;
     }
     return null;
   } catch {
