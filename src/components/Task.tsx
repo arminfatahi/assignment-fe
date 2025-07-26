4c651d6bcd629d59939e5a013ba3a9fca784bc92
arminfatahi <arminfatahi2002@gmail.com>
2025-07-26 15:04:59 +0330

    feat: add task modal

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,19 @@
+import { useModal } from "@/context/ModalContext";
 import { useProjects } from "@/context/ProjectsContext";
 import { Project } from "@/lib/Types";
-import { Box } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import {
+  Box,
+  Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Chip,
+  Stack,
+  IconButton,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { getStatusColor } from "@/lib/utils";
 
 type TaskProps = {
   projectID: number;
@@ -9,18 +21,74 @@ type TaskProps = {
 
 export default function Task({ projectID }: TaskProps) {
   const { projects } = useProjects();
+  const { setOpen } = useModal();
   const project: Project | undefined = projects?.find(
     (p) => p.id === projectID,
   );
 
+  if (!project) {
+    return <Typography>Project not found.</Typography>;
+  }
+
   return (
-    <Box>
-      <Typography id="transition-modal-title" variant="h6" component="h2">
-        {project?.id}
+    <Box p={3} position="relative">
+      <IconButton
+        onClick={() => setOpen(false)}
+        sx={{ position: "absolute", top: 8, right: 8 }}
+      >
+        <CloseIcon />
+      </IconButton>
+
+      <Typography variant="h4" fontWeight="bold" gutterBottom>
+        {project.project_name}
       </Typography>
-      <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-        {project?.status}
+
+      <Stack
+        direction="row"
+        alignItems={"center"}
+        justifyContent="space-between"
+      >
+        <Typography color="text.secondary">Due: {project.due_date}</Typography>
+        <Chip label={project.status} color={getStatusColor(project.status)} />
+      </Stack>
+
+      <Divider sx={{ my: 2 }} />
+
+      <Typography variant="h6" gutterBottom>
+        Tasks
       </Typography>
+
+      <Box
+        sx={{ maxHeight: 300, overflowY: "auto" }}
+        border={1}
+        p={1}
+        borderRadius={2}
+      >
+        <List>
+          {project.tasks.map((task) => (
+            <ListItem
+              key={task.task_id}
+              sx={{
+                color: "text.secondary",
+                border: "2px solid",
+                borderRadius: 2,
+                mb: 2,
+                px: 2,
+                bgcolor: "background.paper",
+              }}
+              secondaryAction={
+                <Chip
+                  label={task.task_status}
+                  color={getStatusColor(task.task_status)}
+                  size="small"
+                />
+              }
+            >
+              <ListItemText color="red" primary={task.task_name} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
     </Box>
   );
 }
